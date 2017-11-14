e615a8e461e6da1d5b12f62e763e894a707be0a9
Joaquin Guillen Melo <joaquin@develop17.com>
2017-11-14 08:36:27 +0100

    Fixed flow types for the example header icon.

--- example/src/HeaderIcon.js
+++ example/src/HeaderIcon.js
@@ -1,15 +1,28 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Animated, Alert, AppState, PushNotificationIOS } from "react-native";
+import { Animated } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+type IconProps = {
+  /** Icon type used. */
+  type: string,
+  /** Indicated whether it's currently in focus/active. */
+  active: boolean,
+  /** Indicated whether the icon is being animated from active to inactive or contrarily. */
+  animating: boolean,
+};
+
+type IconState = {
+  animationDelta: typeof Animated.Value,
+};
 
 class HeaderIcon extends Component<IconProps, IconState> {
   constructor(props: IconProps) {
     super(props);
 
     this.state = {
-      animationDelta: new Animated.Value(0)
+      animationDelta: new Animated.Value(0),
     };
   }
 
@@ -25,7 +38,7 @@ class HeaderIcon extends Component<IconProps, IconState> {
         { toValue: 1 }
       ).start(() =>
         this.setState({
-          animationDelta: new Animated.Value(0)
+          animationDelta: new Animated.Value(0),
         })
       );
     }
@@ -33,32 +46,32 @@ class HeaderIcon extends Component<IconProps, IconState> {
 
   props: IconProps;
 
-  animatedColor = active => {
+  animatedColor = (active: boolean) => {
     const { animationDelta } = this.state;
 
     return animationDelta.interpolate({
       inputRange: [0, 1],
       outputRange: active
-        ? ["rgba(255, 0, 0, 1)", "rgba(100, 100, 100, 1)"]
-        : ["rgba(100, 100, 100, 1)", "rgba(255, 0, 0, 1)"]
+        ? ['rgba(255, 0, 0, 1)', 'rgba(100, 100, 100, 1)']
+        : ['rgba(100, 100, 100, 1)', 'rgba(255, 0, 0, 1)'],
     });
   };
 
-  animatedSize = active => {
+  animatedSize = (active: boolean) => {
     const { animationDelta } = this.state;
 
     return animationDelta.interpolate({
       inputRange: [0, 1],
-      outputRange: active ? [30, 20] : [20, 30]
+      outputRange: active ? [30, 20] : [20, 30],
     });
   };
 
-  stateSize = active => {
+  stateSize = (active: boolean) => {
     return active ? 30 : 20;
   };
 
-  stateColor = active => {
-    return active ? "rgba(255, 0, 0, 1)" : "rgba(100, 100, 100, 1)";
+  stateColor = (active: boolean) => {
+    return active ? 'rgba(255, 0, 0, 1)' : 'rgba(100, 100, 100, 1)';
   };
 
   render() {
@@ -70,7 +83,7 @@ class HeaderIcon extends Component<IconProps, IconState> {
         name={type}
         style={{
           color: animating ? this.animatedColor(active) : this.stateColor(active),
-          fontSize: animating ? this.animatedSize(active) : this.stateSize(active)
+          fontSize: animating ? this.animatedSize(active) : this.stateSize(active),
         }}
         {...other}
       />
